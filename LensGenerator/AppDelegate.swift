717e3f3b3e09ab2f3e450634351ce714f8c666de
Alex Culeva <alexandru.culeva@yopeso.com>
2016-10-12 15:00:24 +0300

    Initial Commit

---
+++ LensGenerator/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LensGenerator
+//
+//  Created by Alexandru Culeva on 10/12/16.
+//  Copyright © 2016 Alexandru Culeva. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
